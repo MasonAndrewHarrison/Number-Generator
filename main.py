778a8b18c77561f3e6a4530eb98acf563142cb2b
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2025-11-07 01:25:56 -0500

    added DCGAN

--- main.py
+++ main.py
@@ -6,50 +6,22 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-
-
-class Discriminator(nn.Module):
-    def __init__(self, in_features):
-        super().__init__()
-        self.disc = nn.Sequential(
-            nn.Linear(in_features, 128),
-            nn.LeakyReLU(0.01),
-            nn.Linear(128, 1),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, x):
-        return self.disc(x)
-
- 
-class Generator(nn.Module):
-    def __init__(self, z_dim, img_dim):
-        super().__init__()
-        self.gen = nn.Sequential(
-            nn.Linear(z_dim, 256),
-            nn.LeakyReLU(0.01),
-            nn.Linear(256, img_dim),
-            nn.Tanh(),
-        )
-
-    def forward(self, x):
-        return self.gen(x)
-
+from model import Discriminator, Generator
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-learning_rate = 3e-4
-z_dimensions = 64
-image_dim = 28 * 28 * 1
+z_dimensions = 100
+image_dim = 64
 batch_size = 64
 num_epochs = 100
 
-disc_model = Discriminator(image_dim).to(device)
-gen_model = Generator(z_dimensions, image_dim).to(device)
+disc_model = Discriminator(1, 64).to(device)
+gen_model = Generator(z_dimensions, 1, 64).to(device)
 
 transforms = transforms.Compose(
     [
+        transforms.Resize(image_dim),
         transforms.ToTensor(),
         transforms.Normalize((0.5), (0.5)),
     ]
@@ -59,49 +31,34 @@ transforms = transforms.Compose(
 dataset = datasets.MNIST(root="dataset/", transform=transforms, download=True)
 loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-opt_disc = optim.Adam(disc_model.parameters(), lr=learning_rate)
-opt_gen = optim.Adam(gen_model.parameters(), lr=learning_rate)
+opt_disc = optim.Adam(disc_model.parameters(), lr=1e-5, betas=(0.5, 0.999))
+opt_gen = optim.Adam(gen_model.parameters(), lr=1e-4, betas=(0.5, 0.999))
 
 criterion = nn.BCELoss()
 
 number = 0
 
 for epoch in range(num_epochs):
-    for i, (real,_) in enumerate(loader):
+    for i, (real,_) in enumerate(loader): 
 
-        z_noise = torch.randn(batch_size, z_dimensions).to(device)
-        real_image = real.view(-1, 784).to(device)
+        z_noise = torch.randn(batch_size, z_dimensions, 1, 1).to(device)
+        real_image = real.to(device)
 
         fake_image = gen_model(z_noise)
-        fake_prediction = disc_model(fake_image.detach()).view(-1)
-        real_prediction = disc_model(real_image).view(-1)
-
         
         #back prob for Discriminator
+        fake_prediction = disc_model(fake_image.detach()).view(-1)
+         
+        
+        real_prediction = disc_model(real_image).view(-1)
+
         disc_loss_fake = criterion(fake_prediction, torch.zeros_like(fake_prediction))
         disc_loss_real = criterion(real_prediction, torch.ones_like(real_prediction))
-
 
         disc_loss = (disc_loss_fake + disc_loss_real) / 2
         disc_model.zero_grad()
         disc_loss.backward()
         opt_disc.step()
-
-        """
-        disc_model.zero_grad()
-        disc_loss_fake.backward()
-        opt_disc.step()
-
-        disc_model.zero_grad()
-        disc_loss_real.backward()
-        opt_disc.step()
-        """
-
-        """
-        disc_loss = -(torch.log(real_prediction).mean() + torch.log(1 - fake_prediction).mean())
-        disc_model.zero_grad()
-        disc_loss.backward()
-        opt_disc.step()"""
 
 
         #back prob for Generator Model  
@@ -114,9 +71,9 @@ for epoch in range(num_epochs):
         if i == 0:
             print(f"{number} | Gen Loss: {gen_loss:.4f} | Disc Loss: {disc_loss_fake:.4f} & {disc_loss_real:.4f}")
             number += 1
-            if number == 98:
+            if number == 5:
         
-                fake_image = fake_image[0].view(28, 28).detach().cpu()
+                fake_image = fake_image[0].view(64, 64).detach().cpu()
                 plt.imshow(fake_image, cmap='gray')
                 plt.show()
 
---
+++ model.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_channel, features):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            nn.Conv2d(img_channel, features, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+
+            self._block(features, features*2, 4, 2, 1),
+            self._block(features*2, features*4, 4, 2, 1),
+            self._block(features*4, features*8, 4, 2, 1),
+
+            nn.Conv2d(features*8, 1, kernel_size=4, stride=2, padding=0),
+            nn.Sigmoid()
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2)
+        )
+    
+    def forward(self, x):
+        return self.disc(x)
+
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, img_channel, features):
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+            self._block(z_dim, features*16, 4, 1, 0),
+            self._block(features*16, features*8, 4, 2, 1),
+            self._block(features*8, features*4, 4, 2, 1),
+            self._block(features*4, features*2, 4, 2, 1),
+            nn.ConvTranspose2d(
+                features * 2, img_channel, kernel_size=4, stride=2, padding=1
+            ),
+            nn.Tanh()
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2)
+        )
+    
+    def forward(self, x):
+        return self.gen(x)
+
+
