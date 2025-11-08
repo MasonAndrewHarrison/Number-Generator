4d7ce59606812c7b0c7eed9a2ae6b08cae9ae659
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2025-11-08 02:25:03 -0500

    gui

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import torch
 import os
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, Button
 from model import Discriminator, Generator
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -13,17 +13,25 @@ gen_model.eval()
 z_noise = torch.randn(1, 100, 1, 1).to(device)
 image = gen_model(z_noise).view(64, 64).detach().cpu()
 
-
-fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.25)
+fig, ax = plt.subplots(figsize=(21, 15))
+plt.subplots_adjust(bottom=0.25, left=0.6)
 
 img_display = ax.imshow(image, cmap="gray")
+slider_list = [0]*100
 
-ax_z = plt.axes([0.25, 0.1, 0.65, 0.03])
-slider_z = Slider(ax_z, 'z[0]', valmin=-5.0, valmax=5.0, valinit=z_noise[0, 0, 0, 0].item())
+for i in range(10):
+    for j in range(10):
 
-ax_z2 = plt.axes([0.25, 0.0, 0.65, 0.03])
-slider_z2 = Slider(ax_z2, 'z[0]', valmin=-5.0, valmax=5.0, valinit=z_noise[0, 1, 0, 0].item())
+        true_index = j+(i*10)
+        print(true_index)
+        ax_z = plt.axes([0.03 + (j/20), 0.9 - (i/10.5), 0.03, 0.067])
+        slider_list[true_index] = Slider(ax_z, true_index+1, valmin=-4.0, valmax=4.0, valinit=z_noise[0, true_index, 0, 0].item(), orientation="vertical")
+
+button_ax = plt.axes([.65, .25, .2, .05])
+button = Button(
+    button_ax,
+    "Reset Z Noise",
+)
 
 def update(val, index):
     z_noise[0, index, 0, 0] = torch.tensor(val, device=device)
@@ -32,7 +40,21 @@ def update(val, index):
     img_display.set_data(new_image)
     fig.canvas.draw_idle()
 
-slider_z.on_changed(lambda x: update(x, 0))
-slider_z2.on_changed(lambda x: update(x, 1))
 
+def on_button_click(event):
+
+    z_noise = torch.randn(1, 100, 1, 1).to(device)
+    
+    for i in range(100):
+        slider_list[i].set_val(z_noise[0, i, 0, 0].item())
+
+    fig.canvas.draw_idle()
+
+
+for i in range(100):
+
+    slider_list[i].on_changed(lambda x, idx=i: update(x, idx))
+
+
+button.on_clicked(on_button_click)
 plt.show()
